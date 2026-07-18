e42fc2f526cece1aa1a380d22981867622eaba2f
曾波涵 <zeng_bohan@163.com>
2026-07-18 08:28:53 +0800

    [MINI-001] feat: add BindPhoneResult VO

---
+++ parking-system/src/main/java/com/jushan/system/vo/BindPhoneResult.java
@@ -0,0 +1,11 @@
+package com.jushan.system.vo;
+
+public class BindPhoneResult {
+    private String phone;
+    private Boolean success;
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public Boolean getSuccess() { return success; }
+    public void setSuccess(Boolean success) { this.success = success; }
+}
